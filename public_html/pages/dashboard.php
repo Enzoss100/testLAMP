7fe719343c7c645573871297bd472e3e47ee57b3
Enzoss100 <lorenzossantillan@gmail.com>
2025-04-04 15:13:52 +0800

    added test database

--- public_html/pages/dashboard.php
+++ public_html/pages/dashboard.php
@@ -1,5 +1,17 @@
 <?php
 	echo "Welcome ", $_POST['name'], ", this is the dashboard of the website",'<br?';
+	// Correct relative path to the DB
+	$db_path = __DIR__ . '/../database/app.db';
 
+	// Create or open the SQLite3 database
+	$db = new SQLite3($db_path);
+
+	// Check if it connected properly
+	if (!$db) {
+		die("Failed to connect to the database.");
+	} 
+	else {
+		echo "Connected successfully!";
+	}
 
 ?>
